557d59a3aaf974fbe5d87a24985633c48e4e597b
GOB52 <GOB52@users.noreply.github.com>
2025-07-15 08:52:39 +0000

    Deploying to gh-pages from @ m5stack/M5Utility@05da21923d6e5724711a996105cbfda62ae7b0c1 🚀

--- compatibility__feature_8cpp.js
+++ compatibility__feature_8cpp.js
@@ -1,7 +1,5 @@
 var compatibility__feature_8cpp =
 [
     [ "delay", "compatibility__feature_8cpp.html#a4d21c95e4e4584fd20525b4070300471", null ],
-    [ "delayMicroseconds", "compatibility__feature_8cpp.html#a7dc4368ab036207b55d64d7f7cf83a00", null ],
-    [ "micros", "compatibility__feature_8cpp.html#a2d4b270efab41768fe9b488056fa7e3a", null ],
-    [ "millis", "compatibility__feature_8cpp.html#a212ab4413419cc8950b40d60700e8802", null ]
+    [ "delayMicroseconds", "compatibility__feature_8cpp.html#a7dc4368ab036207b55d64d7f7cf83a00", null ]
 ];
